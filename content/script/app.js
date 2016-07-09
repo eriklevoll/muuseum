cd8010521eee608b32495d44b7d6ec0e2c8db2ed
eriklevoll <erik.levoll@gmail.com>
2016-07-10 02:25:23 +0300

    many changes
    added images, fixed scripts

--- content/script/app.js
+++ content/script/app.js
@@ -1,5 +1,27 @@
 (function($) {
 
+  var tsarNumbers = 148;
+  var tsarExclude = [3,11];
+
+  var estNumbers  = 324;
+  var estExclude  = [25,144,323];
+
+  var sovietNumbers = 199;
+  var sovietExclude = [1,2,3,53,57,59,76,77,78,101,153,154];
+
+  var germanNumbers = 29;
+  var germanExclude = [3,8,15];
+
+  var currentNumbers = 38;
+  var currentExclude = [];
+
+  var periodNames = ['TSAARIAEG', 'SAKSAAEG', 'EESTIAEG', 'NKOGUDEAEG', 'PRAEGU'];
+  var periodNumbers = [tsarNumbers, germanNumbers, estNumbers, sovietNumbers, currentNumbers];
+  var periodExcludes = [tsarExclude, germanExclude, estExclude, sovietExclude, currentExclude];
+
+
+
+
   var pictureNumbers = [
       "002","003","005","006",
       "007","008","009","010","011","012",
@@ -13,9 +35,32 @@
       "055","056","057","058","059","060",
       "061","062","063","064","065","066",
       "067","068","069","070","071","072"
-    ]
+  ];
 
-  var navButtonsWrap  = $('header').find('.nav-buttons-wrapper'),
+var expositionNumbers = [
+    "1","2","3","4","5","6",
+    "7","9","10","11","12",
+    "13","14","15","16","17","18",
+    "19","20","21","22","23","24",
+    "25","26","28","29","30",
+    "31","32","33","34","35","36",
+    "42",
+    "44","45","46","47","48",
+    "49","50","51","52","53","54",
+    "55","56"
+  ];
+
+var eventsNumbers = [
+    "1","2","3","4","5","6",
+    "7","8","9","10","11","12",
+    "13","14","18",
+    "19","20","21","22","23","24",
+    "25","26","27","28","29"
+  ];
+
+
+  var mainHeader      = $('header'),
+      navButtonsWrap  = mainHeader.find('.nav-buttons-wrapper'),
       videoContainer  = $('.video-container'),
       pageLogo        = $('.page-logo');
       pageLang        = $('.page-language');
@@ -24,6 +69,8 @@
       picturesOverlay     = $('.pictures-page-overlay'),
       contactOverlay      = $('.contact-page-overlay'),
       largePictureOverlay = $('.picture-large-overlay'),
+      picturesBtn         = navButtonsWrap.find('.nav-button-two'),
+      carsBtn             = navButtonsWrap.find('.nav-button-one'),
       contactBody         = contactOverlay.find('.contact-body'),
       contactNav          = contactOverlay.find('ul'),
       sideNavUl           = picturesOverlay.find('ul'),
@@ -59,18 +106,30 @@
     var cols_count = children.filter(function() {
       return $(this).css('display') !== 'none';
     }).length;
-    for (i = 0; i < pictureNumbers.length; i++) {
-      children.eq(i%cols_count).append('<img src="content/images/gallery/IMG_0' + pictureNumbers[i] + '.jpg"/>');
+    for (i = 0; i < expositionNumbers.length; i++) {
+      children.eq(i%cols_count).append('<img src="content/images/ekspositsioon/' + expositionNumbers[i] + '.JPG"/>');
+    }
+    for (i = 0; i < eventsNumbers.length; i++) {
+      children.eq(i%cols_count).append('<img src="content/images/events/' + eventsNumbers[i] + '.jpg"/>');
     }
   };
 
-  var distributePictureCols = function() {
+  var distributePictureCols = function(period) {
     var children = picturesBody.children();
+    children.html('');
     var cols_count = children.filter(function() {
       return $(this).css('display') !== 'none';
     }).length;
-    for (i = 0; i < pictureNumbers.length; i++) {
-      children.eq(i%cols_count).append('<img src="content/images/gallery/IMG_0' + pictureNumbers[i] + '.jpg"/>');
+    var zeros;
+    var folder = periodNames[period];
+    var number = periodNumbers[period];
+    var exclude = periodExcludes[period];
+    for (i = 0; i < number; i++) {
+      if (jQuery.inArray(i,exclude) != -1) continue;
+      if (i < 10) zeros = "000";
+      else if (i < 100) zeros = "00";
+      else if (i < 1000) zeros = "0";
+      children.eq(i%cols_count).append('<img src="content/images/ajastud/'+ folder +'/IMG_' + zeros + i + '.jpg"/>');
     }
   };
 
@@ -86,10 +145,21 @@
     }
   };
 
-  checkMobile();
-  distributePictureCols();
-  distributeCarCols();
+  picturesBtn.on('click', function() {
+    if (picturesOverlay.hasClass('.no-display')) {
+      picturesOverlay.removeClass('.no-display');
+      distributePictureCols(0);
+    }
+  });
 
+  carsBtn.on('click', function() {
+    if (carsOverlay.hasClass('.no-display')) {
+      carsOverlay.removeClass('.no-display');
+      distributeCarCols();
+    }
+  });
+
+  checkMobile();
 
   var fixVideoSize = function() {
     windowWidth = $(window).width();
@@ -130,10 +200,18 @@
   };
 
   sideNavUl.children().on('click', function() {
-    // console.log();
+    if ($(this).hasClass('.active')) return;
     $(this).siblings().css({'background':'none'});
-    console.log($(this).html());
+    var period    = $(this).attr('data-title');
+    // var periodNumbers = $(this).attr('data-number');
+    // var periodExclude = $(this).attr('data-exclude');
+    console.log(period);
+    // console.log(periodNumbers);
+    // console.log(periodExclude);
+    distributePictureCols(period);
     $(this).css({'background':'rgba(250,250,250,0.2)'});
+    $(this).siblings().removeClass('.active');
+    $(this).addClass('.active');
   });
 
   picturesOverlay.scroll(function(){
@@ -150,18 +228,18 @@
   });
 
   thumbDiv.on('click', function() {
-    var source = $(this).find('img').attr('src');
-    largePicContainer.css({
-      'background': 'url('+ source +') no-repeat center center'
-    });
-    largePictureOverlay.fadeIn(200,function() {});
+    // var source = $(this).find('img').attr('src');
+    // largePicContainer.css({
+    //   'background': 'url('+ source +') no-repeat center center'
+    // });
+    // largePictureOverlay.fadeIn(200,function() {});
   });
 
   largePicContainer.find('.large-picture-close').on('click', function() {
-    imagesPage();
+    // imagesPage();
   });
 
-  navButtonsWrap.children().on('click', function() {
+  navButtonsWrap.children().children().on('click', function() {
     var overlayName = '.' + $(this).attr('data-overlay');
     navButtonsWrap.css({'z-index': '1'});
     $(overlayName).fadeIn(300,function() {
